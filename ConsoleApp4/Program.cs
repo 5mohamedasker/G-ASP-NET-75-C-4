97fbbc5067d4d4e5fbd560f38122b300122fc462
5mohamedasker <5mohamedasker@gmail.com>
2026-07-29 18:31:08 +0300

    Question02

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp4
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp4
 {
     internal class Program
     {
@@ -10,6 +12,14 @@
             ////Print the second price(index 1).
             //double[] prices = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(prices[1]);
+            #endregion
+            #region Question02
+            ////.2Create a 2x2 multidimensional array int[,]
+            ////shelfCopies where shelf 0 has 3, 5 copies
+            ////and shelf 1 has 1, 4 copies.
+            ////Print the number of copies on shelf 1, slot 0.
+            //int[,] arr = { {3,5} , {1,4} };
+            //Console.WriteLine(arr[1, 0]);
             #endregion
         }
     }
